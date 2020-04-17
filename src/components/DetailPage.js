01f80c15a96a9c8ff72dacfe025faa43de1a5f7b
fabbud <fabbud@gmail.com>
2020-04-17 12:56:44 +0100

    super setup final

---
+++ src/components/DetailPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DetailPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Detail Page</h1>;
+  }
+}
+export default DetailPage;
---
+++ src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Homepage</h1>;
+  }
+}
+export default Homepage;
