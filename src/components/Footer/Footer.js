5c444c1ae3a863618df70ef0e6e4862376938954
fabbud <fabbud@gmail.com>
2020-05-06 11:42:08 +0100

    adjustments in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,21 +4,37 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <div className="footer">
-      <div>
-        <Link to="/aboutus">About Us</Link>
-        <span>Created by </span>
-        <a href="https://github.com/fabbud">Fernanda </a>
-        <span>|</span>
-        <a href="https://github.com/isadorahaashillebrand"> Isadora </a>
-        <span>|</span>
-        <a href="https://github.com/MargSousa"> Margarida </a>
-        <span>|</span>
-        <a href="https://github.com/RoxyPursey"> Roxy</a>
-        <span> - </span>
-        <a href="https://www.wildcodeschool.com">Wild Code School</a>
+    <footer className="footer">
+      <div className="imgFooter-div">
+        <img
+          className="logo-img"
+          src="https://i.imgur.com/vLhf37l.png"
+          alt="Logo"
+        />
       </div>
-    </div>
+      <div className="about-wcs">
+        <div className="aboutus">
+          <Link to="/aboutus">About Us </Link>
+        </div>
+        <div className="team">
+          <a href="https://github.com/fabbud">Fernanda </a>
+          <span>|</span>
+          <a href="https://github.com/isadorahaashillebrand"> Isadora </a>
+          <span>|</span>
+          <a href="https://github.com/MargSousa"> Margarida </a>
+          <span>|</span>
+          <a href="https://github.com/RoxyPursey"> Roxy</a>
+        </div>
+        <div className="wcs">
+          <a href="https://www.wildcodeschool.com">
+            Wild Code School{' '}
+            <span role="img" aria-label="drink">
+              🍸
+            </span>
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 }
 
